0b78bc32941f7044b86056397b4e93b8d32e0d11
Mateus Rovari <mateus@liveonsolutions.com>
2018-12-29 14:59:14 -0200

    setup third activitylifycicle project

---
+++ ActivityLifeCycle3/app/src/main/java/br/com/softblue/android/MainActivity2.java
@@ -0,0 +1,16 @@
+package br.com.softblue.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity2 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity2);
+    }
+}
