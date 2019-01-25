08d53a76485153b99ff8f2eca96f98a5a2c2cc3f
Mateus Rovari <mateus@liveonsolutions.com>
2019-01-25 08:43:14 -0200

    setup alarm project

---
+++ Alarm/app/src/main/java/com/mateusrovari/alarm/AlarmReceiver.java
@@ -0,0 +1,14 @@
+package com.mateusrovari.alarm;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Alarme disparado!", Toast.LENGTH_SHORT).show();
+    }
+}
